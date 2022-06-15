62198259f2794eec4b726ec1d4e5a711d606aee7
Maddy Tan <maddytan7@gmail.com>
2022-06-15 15:02:45 -0400

    includes form edit and form create

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,17 +3,20 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers.map((interviewer) => {
+  const interviewers = Array.isArray(props.interviewers) && props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
         // selected={interviewer.id === value}
         // setInterviewer={() => onChange(interviewer.id)} 
       />
     );
   });
+
 
   return (
   <section className="interviewers">
@@ -22,3 +25,10 @@ export default function InterviewerList(props) {
   </section>
 );
 };
+
+
+{/* <Filter>
+  { product.color?.map((c) => (
+    <FilterColor color = {c} key = {c} />
+  ))};
+</Filter> */}
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,5 +153,6 @@ storiesOf("Button", module)
       .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
       .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
       .add("Status", () => <Status message="Deleting" />)
-      .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
-
+      .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+      .add("Form Edit", () => <Form student="Maddy" interviewer={4} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+      .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
