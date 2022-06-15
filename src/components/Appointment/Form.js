e16cba3f7e40ef58d1d54b46384ce420171ba2a2
Maddy Tan <maddytan7@gmail.com>
2022-06-15 16:08:58 -0400

    includes updated form with props passed in and cancel function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
+
 export default function Form(props){
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  // Add a reset() function to the Form component that calls setStudent("") and setInterviewer(null).
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
@@ -12,19 +28,23 @@ export default function Form(props){
           name={props.name}
           type="text"
           placeholder="Enter Student Name"
+          value={student}
+          onChange={(event) => setStudent(event.target.value)}  
           /*
             This must be a controlled component
             your code goes here
           */
         />
       </form>
-      <InterviewerList 
-        {... props.interviewers}
-      />
+        <InterviewerList 
+        interviewers={props.interviewers} 
+        value={interviewer}
+        onChange={(event) => setInterviewer(event)} 
+        />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button danger onClick={cancel}>Cancel</Button>
         <Button confirm onClick={props.onSave}>Save</Button>
       </section>
     </section>
