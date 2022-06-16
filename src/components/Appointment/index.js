1d6266561a9dbeaf885cd4b960093f38d2f6a6ed
Maddy Tan <maddytan7@gmail.com>
2022-06-16 12:02:59 -0400

    includes working appointment show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./styles.scss";
 
-export default function Appointment() {
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import Status from 'components/Appointment/Status';
+import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
+
+export default function Appointment(props) {
   return (
-  <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) :
+        <Empty />
+      }
+    </article>
   );
 };
